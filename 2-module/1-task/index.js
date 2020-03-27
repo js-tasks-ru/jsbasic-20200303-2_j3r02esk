5b7ba5256cb7a943478324fabecb745a7663f7be
j3r02esk <j3r02esk@tutanota.com>
2020-03-27 11:40:19 +0100

    task-1 - task-3

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,8 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-
+    if ('John' in salaries && 'Ann' in salaries && 'Pete' in salaries) {
+        return salaries.John+salaries.Ann+salaries.Pete;
+    }
+    else { return 0; }
 }
