7e25c43ce47e9b75ea37a15f115282540920fbbb
j3r02esk <j3r02esk@tutanota.com>
2020-03-27 07:05:49 +0100

    task-0 task-1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,17 @@
  * @param {number} n
  * @returns {number}
  */
-function factorial(n) {
 
+
+function factorial(n) {
+    if(n == 0 || n == 1) {
+        var f = 1;
+    } else {
+        var f = parseInt(n);
+        for(var i = 1; i < n; i++) {
+            f *= i;
+        }
+    }
+    return f;
 }
+
