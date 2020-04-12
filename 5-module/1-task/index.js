36ef7c8ff9b7876d606ca7324b2ce56bcd208724
j3r02esk <j3r02esk@tutanota.com>
2020-04-12 19:49:59 +0200

    4-module/task-1 and 5-module/task-2

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -20,32 +20,3 @@ function highlight(table) {
 	}
 	
 }
-
-
-function highlight(table) {
-   let elements = table.querySelectorAll(' tr > td:last-child');
-   for (let i = 1; i < table.rows.length; i++) {
-      if (elements[i].dataset.available === 'true') {
-         table.rows[i].classList.add('available');
-      }
-      else if (elements[i].dataset.available === 'false') {
-         table.rows[i].classList.add('unavailable');
-      }
-      else if
-         (elements[i].hasAttribute('data-available') === false) {
-         table.rows[i].hidden = true;
-      }
-   }
-   for (let i = 1; i < table.rows.length; i++) {
-      if (table.rows[i].cells[2].innerHTML === 'm') {
-         table.rows[i].classList.add('male');
-      } else {
-         table.rows[i].classList.add('female');
-      }
-   }
-   for (let i = 1; i < table.rows.length; i++) {
-      if (table.rows[i].cells[1].innerHTML < '18') {
-         table.rows[i].style.textDecoration = 'line-through';
-      }
-   }
-}
