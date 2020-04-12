e1f417867ab8f95410c85dc6598257e999aaebea
j3r02esk <j3r02esk@tutanota.com>
2020-04-12 19:48:06 +0200

    4-module/task-1 and 5-module/task-2

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,13 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+	let ul = document.createElement('ul');
+
+	for ( i=0; i < friends.length; i++ ) {
+			let li = document.createElement('li');
+			ul.appendChild(li)
+			li.innerHTML = friends[i].firstName + ' ' + friends[i].lastName;	
+		}
+
+	return ul;
 }
--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,49 @@
  * @param {Element} table
  */
 function highlight(table) {
+
+	for ( i=1; i < table.rows.length; i++ ) {
+		let age		= table.rows[i].cells[1];
+		let gender	= table.rows[i].cells[2];
+		let status	= table.rows[i].cells[3];
+
+
+		if (age.innerHTML < 18)							age.parentElement.setAttribute('style', 'text-decoration: line-through');
+		if (gender.innerHTML == 'm')					gender.parentElement.classList.add('male');
+		if (gender.innerHTML == 'f')					gender.parentElement.classList.add('female');
+		if (status.dataset.available == 'true')			status.parentElement.classList.add('available');
+		if (status.dataset.available == 'false')		status.parentElement.classList.add('unavailable');
+		if (status.dataset.available == undefined)		status.parentElement.setAttribute('hidden', '');
+
+	}
+	
+}
+
+
+function highlight(table) {
+   let elements = table.querySelectorAll(' tr > td:last-child');
+   for (let i = 1; i < table.rows.length; i++) {
+      if (elements[i].dataset.available === 'true') {
+         table.rows[i].classList.add('available');
+      }
+      else if (elements[i].dataset.available === 'false') {
+         table.rows[i].classList.add('unavailable');
+      }
+      else if
+         (elements[i].hasAttribute('data-available') === false) {
+         table.rows[i].hidden = true;
+      }
+   }
+   for (let i = 1; i < table.rows.length; i++) {
+      if (table.rows[i].cells[2].innerHTML === 'm') {
+         table.rows[i].classList.add('male');
+      } else {
+         table.rows[i].classList.add('female');
+      }
+   }
+   for (let i = 1; i < table.rows.length; i++) {
+      if (table.rows[i].cells[1].innerHTML < '18') {
+         table.rows[i].style.textDecoration = 'line-through';
+      }
+   }
 }
